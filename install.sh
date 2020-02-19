ed65595f45736a3b8c25173c0f230fafb71994d0
Adrisson Samersla <adrissonsamersla@gmail.com>
2020-02-19 15:59:48 -0300

    mkdir of Dados_In and Dados_Out, required during execution.

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 # Instala primeiramente as dependências em python
 pip3 install -r requirements.txt 
 
+mkdir Dados_In
+mkdir Dados_Out
+
 # Instala a biblioteca BUFRLIB
 cd Parser
 python3 setup.py install
